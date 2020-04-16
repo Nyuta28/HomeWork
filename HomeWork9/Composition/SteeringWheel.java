be06c08bd667bc12de4c5f6d1f5590f4cdd53107
Nyuta28 <33639705+Nyuta28@users.noreply.github.com>
2020-04-16 16:37:32 +0300

    Create SteeringWheel.java

---
+++ HomeWork9/Composition/SteeringWheel.java
@@ -0,0 +1,16 @@
+package HomeWork9.Encapsulation.Composition;
+
+public class SteeringWheel {
+
+    private String direction;
+
+    public SteeringWheel(String direction){
+        this.direction = direction;
+    }
+
+    public String getDirection(){
+        return direction;
+    }
+
+}
+
