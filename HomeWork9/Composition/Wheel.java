5c453c70600d1b516fdc8a9cad8654aef87494a7
Nyuta28 <33639705+Nyuta28@users.noreply.github.com>
2020-04-16 16:38:37 +0300

    Create Wheel.java

---
+++ HomeWork9/Composition/Wheel.java
@@ -0,0 +1,14 @@
+package HomeWork9.Encapsulation.Composition;
+
+public class Wheel {
+    private String direction;
+
+    public Wheel(String direction){
+        this.direction = direction;
+    }
+
+    public String getDirection(){
+        return direction;
+    }
+
+}
