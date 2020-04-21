3a95da8d745cfbc5ee0a55da0722a891dc14f68f
Nyuta28 <33639705+Nyuta28@users.noreply.github.com>
2020-04-21 16:21:51 +0300

    Create ElementOfNature.java

---
+++ HomeWork10/Singleton/ElementOfNature.java
@@ -0,0 +1,8 @@
+package HomeWork10;
+
+public interface ElementOfNature {
+        static String AIR = "air";
+        static String WATER = "water";
+        static String EARTH = "earth";
+        static String FIRE = "fire";
+    }
