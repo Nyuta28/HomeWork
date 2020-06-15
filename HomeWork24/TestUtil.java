7c444abdd9f01543f363b3cbe1fe08d900035ccb
Nyuta28 <33639705+Nyuta28@users.noreply.github.com>
2020-06-15 22:08:57 +0300

    Create TestUtil.java

---
+++ HomeWork24/TestUtil.java
@@ -0,0 +1,29 @@
+package HomeWork24.TestListeneOnFailure;
+
+import HomeWork24.TestListeneOnFailure.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class TestUtil extends TestBase {
+
+    public static String screenshotPath;
+    public static String screenshotName;
+
+    public static void captureScreenshot() throws IOException {
+
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        Date d = new Date();
+        screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+        FileUtils.copyFile(scrFile,
+                new File(System.getProperty("user.dir") + "/screenshot/" + screenshotName));
+
+    }
+
+}
